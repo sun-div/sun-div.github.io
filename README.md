ff37914ec4cc3a9678409c51688a86bf7d234cb1
sun-div <divsundar9@gmail.com>
2019-07-15 15:59:00 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# sun-div.github.io" 
---
+++ myScript.js
@@ -0,0 +1,24 @@
+var img = document.createElement("img");
+
+img.src = "first_project.JPG";
+var src = document.getElementById("x");
+
+src.appendChild(img);
+
+
+var img = document.createElement("img2");
+
+img2.src = "first_project.JPG";
+var src = document.getElementById("x");
+
+src.appendChild(img2);
+
+
+
+
+// var index=0;
+//    function changeImg(){
+//      [].forEach.call(document.images,function (v,i) { document.images[i].hidden = i!==index});
+//      index = (index+1) % document.images.length;
+//    }
+//    window.onload = function () {setInterval(changeImg, 1000)};
